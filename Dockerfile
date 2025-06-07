f464912d213c2704c0b6fc750697d264286aa1aa
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2025-06-07 23:17:30 +0530

    feat: added DOCKERFILE

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM ghcr.io/puppeteer/puppeteer:24.10.0
+
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+ENV PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
+
+
+WORKDIR /app
+
+COPY package*.json .
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+EXPOSE 3000
+
+CMD ["npm","run","start"]
--- src/utils/htmlToImage.ts
+++ src/utils/htmlToImage.ts
@@ -4,14 +4,12 @@ export const htmlToPng = async (html: string): Promise<Buffer> => {
   const browser = await puppeteer.launch({
     headless: true,
     ignoreDefaultArgs: ["--disable-extensions"],
-    args: ["--no-sandbox", "--use-gl=egl", "--disable-setuid-sandbox"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: "networkidle0" });
-  await page.setUserAgent(
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
-  );
+
   await page.evaluateHandle("document.fonts.ready");
   const element = await page.$(".ticket");
   if (!element) throw new Error("Ticket element not found");
